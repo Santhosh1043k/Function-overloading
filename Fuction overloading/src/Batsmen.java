2f6e424c16e9a51d3e77bd5a9fee11b511d43718
Santhosh1043k <159894976+Santhosh1043k@users.noreply.github.com>
2024-02-14 14:39:16 +0530

    Add files via upload

---
+++ Fuction overloading/src/Batsmen.java
@@ -0,0 +1,9 @@
+public class Batsmen {
+    public void printRun(){
+        System.out.println("batsmen scored 0");
+    }
+    public void printRun(int run){
+        System.out.println("batsmen scored "+run);
+    }
+
+}
---
+++ Fuction overloading/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Batsmen batsmen = new Batsmen();
+        batsmen.printRun();
+        batsmen.printRun(100);
+    }
+}
